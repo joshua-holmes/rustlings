9660ae1ffd8b768fcb1262b72d4f645cb042d622
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2023-01-26 18:33:16 -0800

    Finish modules

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,17 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(quantity: i32) -> i32 {
+    let mut price: i32 = if quantity > 40 {
+        1
+    } else {
+        2
+    };
+
+    price * quantity
+}
 
 // Don't modify this function!
 #[test]
